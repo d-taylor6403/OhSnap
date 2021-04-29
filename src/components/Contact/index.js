df5925d0fe8e6de9e511a6fc3d2e0914a219c94e
dannitay <dannielletaylor@outlook.com>
2021-04-28 19:33:26 -0500

    adding submit function for contact form using useState

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React , {useState} from 'react';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    const {name, email, message} = formState;
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState)
+    }
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
 
     return (
         <section>
             <h1>Contact Me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name"  defaultValue={name} onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange}/>
                 </div>
                 <button type="submit">Submit</button>
             </form>
