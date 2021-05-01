ff027d9b693119c6d93527d18307c11e5991e11b
dannitay <dannielletaylor@outlook.com>
2021-04-30 22:35:24 -0500

    adding conditional styling to navbar

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,9 @@
 import React , {useState} from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    const [errorMessage, setErrorMessage] = useState('');
     const {name, email, message} = formState;
 
     function handleSubmit(e) {
@@ -10,8 +12,27 @@ function ContactForm() {
     }
 
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value})
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            //isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            }else{
+                setErrorMessage('');
+            }
+        }else{
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`)
+            }else{
+                setErrorMessage('');
+            }
+        }
+        if(!errorMessage){
+        setFormState({...formState, [e.target.name]: e.target.value});
+        }
     }
+
 
     return (
         <section>
@@ -19,16 +40,21 @@ function ContactForm() {
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name"  defaultValue={name} onChange={handleChange}/>
+                    <input type="text" name="name"  defaultValue={name} onBlur={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" defaultValue={email} onChange={handleChange}/>
+                    <input type="email" name="email" defaultValue={email} onBlur={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange}/>
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange}/>
                 </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
                 <button type="submit">Submit</button>
             </form>
         </section>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,6 +6,8 @@ function Nav(props) {
     categories = [],
     setCurrentCategory,
     currentCategory,
+    contactSelected,
+    setContactSelected
   } = props;
 
 
@@ -23,26 +25,27 @@ function Nav(props) {
       </h2>
       <nav>
         <ul className="flex-row">
-          <li className="mx-2">
-            <a data-testid="about" href="#about">
+          <li className={`mx-2 ${contactSelected && "navActive"}`}>
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
-          <li className={"mx-2"}>
-            <span onClick={() => handleClick('Contact')}>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>
               Contact
             </span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name
+                currentCategory.name === category.name && !contactSelected &&  `navActive`
                 }`}
               key={category.name}
             >
               <span
                 onClick={() => {
                   setCurrentCategory(category);
+                  setContactSelected(false)
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
